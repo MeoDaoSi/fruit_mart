30bab6360a9fcbf078c08ad9dd7d6c5074cd20c8
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2022-05-12 12:50:03 +0700

    make11

--- main.js
+++ main.js
@@ -6,17 +6,3 @@ setInterval(function(){
         counter = 1;
     }
 },5000);
-
-let loadMoreBtn = document.querySelector("#more");
-let currentItem = 8;
-loadMoreBtn.onclick = () =>{
-    let boxes = [...document.querySelectorAll('.items-wrapper .item')];
-    for ( var i = currentItem ; i < currentItem + 4 ; i++ ){
-        boxes[i].style.display = 'flex';
-    }
-    currentItem+=4;
-
-    if ( currentItem >= boxes.length ){
-        loadMoreBtn.style.display = 'none';
-    }
-}
