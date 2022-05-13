1b3c4745cce61b3c0dc4243691aa38a7f06954bd
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2022-05-13 16:34:02 +0700

    update-cart-js

--- cart.js
+++ cart.js
@@ -1,6 +1,6 @@
 let carts = document.querySelectorAll('.add-cart');
 
-let product = [
+let products = [
     {
         name: 'Khổ qua - 350gr',
         tag: 'khoqua350gr',
@@ -101,7 +101,8 @@ let product = [
 
 for ( let i=0 ; i < carts.length ; i++ ){
     carts[i].addEventListener('click', () => {
-        cartNumbers();
+        cartNumbers(products[i]);
+        totalCost(products[i]);
     });
 }
 
@@ -112,7 +113,7 @@ function onLoadCartNumbers(){
     }
 }
 
-function cartNumbers(){
+function cartNumbers( product ){
     let productNumbers = localStorage.getItem('cartNumbers');
 
     productNumbers = parseInt(productNumbers);
@@ -125,5 +126,89 @@ function cartNumbers(){
         localStorage.setItem('cartNumbers', 1);
         document.querySelector(' .cart span').textContent = 1;
     }
+
+    setItems(product);
 }
+function setItems(product){
+    let cartItems = localStorage.getItem('productInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if ( cartItems != null ){
+        if ( cartItems[product.tag] == undefined ){
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    }
+    else{
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    
+    localStorage.setItem("productInCart",JSON.stringify(cartItems));
+}
+
+function totalCost(product){
+    let cartCost = localStorage.getItem('totalCost');
+
+    if ( cartCost != null ){
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+    }else{
+        localStorage.setItem("totalCost", product.price);
+    }
+    console.log(cartCost);
+}
+
+function displayCart(){
+    let cartItems = localStorage.getItem("productInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem('totalCost');
+
+    if ( cartItems && productContainer ){
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map( item => {
+            productContainer.innerHTML += `
+            <tbody class="products">
+                    <tr>
+                        <td><img src="./assets/img/img-trangchu/${item.tag}.jpg" alt=""></td>
+                        <td><h5>${item.name}</h5></td>
+                        <td><h5>${item.price}000đ</h5></td>
+                        <td>${item.inCart}</td>
+                        <td><h5>
+                            ${item.price * item.inCart},000đ;
+                        </h5></td>
+                        <td><a href=""><i class="ti-trash"></i></a></td>
+                    </tr>
+            </tbody>
+            `;
+        });
+        let basket = document.querySelector(".cart-bottom-container");
+        basket.innerHTML += `
+            <div class="row">
+                <div class="cart-bottom-note">
+                    <h5>Ghi chú:</h5>
+                    <textarea name="note" id="note" cols="70" rows="7"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="cart-bottom-buy">
+                    <h5>Giỏ hàng</h5>
+                    <div class="cart-bottom-container">
+                        <h6>Thành tiền</h6>
+                        <h6>${cartCost}.000 đ</h6>
+                    </div>
+                    <button>THANH TOÁN</button>
+                </div>
+            </div>
+        `
+    }
+}
+
 onLoadCartNumbers();
+displayCart();
